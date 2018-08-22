e7f8a6e3d1a94c000f0323d936a20331d8a33c0b
jonas <ellvtr@users.noreply.github.com>
2018-08-22 11:53:33 +0200

    Make deploy procedure for angular app; 2
    Issue #20.
    It now works with `docker-compose` which is faster and easier to work with.
    Added `docker-compose.yml` and moved express server to dir `serv-dist`.
    Removed redundant shell scripts from previous commit.

--- angular-app/serv-dist/serv.js
+++ angular-app/serv-dist/serv.js
@@ -1,13 +1,16 @@
+// To be run from parent directory with `node serv-dist/serv.js`
+
 const express = require('express'),
       cors = require('cors');
 
 const app = express(),
-      port = 4200;
+      port = 3000;
 
 // Allow all cors (cross domain requests):
 app.use(cors());
 
-app.use(express.static('my-app'));
-app.use('/home', express.static('my-app'));
+// Path relative to parent directory:
+app.use(express.static('dist/my-app'));
+app.use('/home', express.static('dist/my-app'));
 
 app.listen(port, f=>{ console.log(`app listening on port ${port}`) });
