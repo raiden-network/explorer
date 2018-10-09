4fdca86151049dde43a57736ea21e7bb5a927be5
Paul Lange <palango@gmx.de>
2018-10-09 15:12:34 +0200

    Fix some bugs in the topology code

--- backend/metrics_backend/model/channel_view.py
+++ backend/metrics_backend/model/channel_view.py
@@ -18,36 +18,47 @@ class ChannelView:
         channel_id: ChannelIdentifier,
         participant1: Address,
         participant2: Address,
-        deposit: int = 0
     ) -> None:
         assert is_checksum_address(participant1)
         assert is_checksum_address(participant2)
 
-        self.self = participant1
-        self.partner = participant2
+        self.participant1 = participant1
+        self.participant2 = participant2
 
-        self._deposit = deposit
-        self.state = ChannelView.State.OPENED
         self.channel_id = channel_id
+        self._deposit_p1 = 0
+        self._deposit_p2 = 0
+        self.state = ChannelView.State.OPENED
 
-    def update_capacity(
+    def update_deposit(
         self,
+        participant: Address,
         deposit: int = None,
     ):
         if deposit is not None:
-            self._deposit = deposit
+            if participant == self.participant1:
+                self._deposit_p1 = deposit
+            elif participant == self.participant2:
+                self._deposit_p2 = deposit
 
     def update_state(self, new_state: State):
         if new_state is not None:
             self.state = new_state
 
     @property
-    def deposit(self) -> int:
-        return self._deposit
+    def deposit_p1(self) -> int:
+        return self._deposit_p1
+
+    @property
+    def deposit_p2(self) -> int:
+        return self._deposit_p2
 
     def __repr__(self):
-        return '<ChannelView from={} to={} deposit={}>'.format(
-            self.self,
-            self.partner,
-            self.deposit,
+        return '<ChannelView id={} p1={} p2={} state={} deposit_p1={} deposit_p2={}>'.format(
+            self.channel_id,
+            self.participant1,
+            self.participant2,
+            self.state,
+            self.deposit_p1,
+            self.deposit_p2,
         )
