e81b0600671750d383d9d3e1024fe14429628833
manuelwedler <manuel.wedler@googlemail.com>
2019-09-24 13:59:16 +0200

    Fixes the 'show network visualization' button to work properly

--- frontend/src/app/components/buttons-section/buttons-section.component.ts
+++ frontend/src/app/components/buttons-section/buttons-section.component.ts
@@ -9,4 +9,8 @@ export class ButtonsSectionComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  scrollToNetwork() {
+    document.querySelector('#network').scrollIntoView();
+  }
 }
