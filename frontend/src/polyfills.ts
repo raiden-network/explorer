e124b8830bf63b4896b3b53de6783f2902c16d5e
manuelwedler <manuel.wedler@googlemail.com>
2020-11-25 16:15:22 +0100

    Fix 'global is not defined' error by providing a polyfill
    The recent update of deep-equal from 1.0.1 to 1.1.1 caused this error

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -65,6 +65,8 @@
  */
 // (window as any).__Zone_enable_cross_context_check = true;
 
+(window as any).global = window;
+
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
